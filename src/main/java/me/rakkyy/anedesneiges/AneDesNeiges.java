ed2cfbe7d0e4f5c494ec994770a7dc259b5f39c0
Rakkyy <karaakaashi@gmail.com>
2023-02-05 23:00:56 +0100

    Initial commit

---
+++ src/main/java/me/rakkyy/anedesneiges/AneDesNeiges.java
@@ -0,0 +1,17 @@
+package me.rakkyy.anedesneiges;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AneDesNeiges extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

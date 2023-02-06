113a01b47094a146a62256d66945a0a0c14e14be
Rakkyy <karaakaashi@gmail.com>
2023-02-06 14:50:09 +0100

    Test GitHub(ide) + Starting

--- src/main/java/me/rakkyy/anedesneiges/AneDesNeiges.java
+++ src/main/java/me/rakkyy/anedesneiges/AneDesNeiges.java
@@ -8,6 +8,7 @@ public final class AneDesNeiges extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        this.getServer().getLogger().info("[AneDesNeiges] Plugin AneDesNeiges correctement activé");
     }
 
     @Override
